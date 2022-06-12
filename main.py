21e65c9f97f6839d4c3b983f957627eb7f3d0aef
Andrew <paintstone@gmail.com>
2022-06-12 00:03:07 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press ⇧F10 to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import DA420_Project9_test
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    
+    
+    
+def DA420_Project9_test():
+    """
+    testing if I can call or out put the functions of a .ipynb
+    :return:
+    """
+    
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
